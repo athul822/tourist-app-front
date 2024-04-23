ea985491f7d3783495a8c6ee19bed4cd477de350
athulssiorbit <athulss@iorbit-tech.com>
2024-04-23 15:07:20 +0530

    pro api

--- src/actions/api.js
+++ src/actions/api.js
@@ -2,8 +2,8 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api/';
-// const BASE_URL = 'https://spectrum-freckle-scabiosa.glitch.me/api';
+// const BASE_URL = 'http://localhost:8000/api/';
+const BASE_URL = 'https://spectrum-freckle-scabiosa.glitch.me/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
